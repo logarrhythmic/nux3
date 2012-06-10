b10aaccaa9e133a0ea3c6d8dea324f11810f8c3f
Felix Bade <flix.bade@gmail.com>
2012-06-10 16:21:15 +0300

    s//\n/g

--- loop.py
+++ loop.py
@@ -62,6 +62,7 @@ def cycle(connection, channels):
 					if quote:
 						new = re.sub(msg[2:msg[2:].find('/')+2],
 						msg[msg[2:].find('/')+3:], ' '.join(quote.split()[1:]), 1)
+						new = new.replace('\n', '')
 						
 						print quote
 						print msg
@@ -83,6 +84,7 @@ def cycle(connection, channels):
 						new = re.sub(msg[2:msg[2:].find('/')+2],
 						msg[msg[2:].find('/')+3:msg.rfind('/')],
 						' '.join(quote.split()[1:]))
+						new = new.replace('\n', '')
 
 						print quote
 						print msg
